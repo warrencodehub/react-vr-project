17cace1e104fd7cb9f946c1bfbe53c8acc594045
Miraflor <wmiraflor@hartmannph.com>
2024-06-14 16:48:35 +0800

    test updates

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance } from "react-360-web";
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -12,12 +12,14 @@ function init(bundle, parent, options = {}) {
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('react_vr', { /* initial props */ }),
+    r360.createRoot("react_vr", {
+      /* initial props */
+    }),
     r360.getDefaultSurface()
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL("example-ps-2.jpg"));
 }
 
-window.React360 = {init};
+window.React360 = { init };
